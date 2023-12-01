556e4d51960d7ec815d5cd027357197a1e781bca
Humberto Medina <h.medina@aerofluids.org>
2023-12-01 18:44:13 +0000

    combined induced velocity calculation (secant method updated)

--- prototyping.jl
+++ prototyping.jl
@@ -5,8 +5,8 @@ file = "naca001264-il-200000-n5.csv"
 cl, cd = load_aerofoil(file; startline=12)
 a = [-0.22:0.001:0.22;]
 
-plot(rad2deg.(a), cl.(a))
-plot!(rad2deg.(a), cd.(a))
+# plot(rad2deg.(a), cl.(a))
+# plot(rad2deg.(a), cd.(a))
 
 diameter = 25.40e-2
 radius = diameter/2
@@ -14,47 +14,36 @@ chord = 0.15*radius
 nb = 2
 rpm = 3000
 omega = rpm*(2π/60)
-T = 3.5
 v_tip = omega*radius
 # J = V/(nD) # n in rev/sec
 n = rpm*(1/60)
 J = 0.6
 vc = J*n*diameter
-# vc = 100
-
+# vc = 0
 ρ = 1.225
-σ = sigma(chord, radius, nb)
 
-induced_velocity(Vc, T, ρ, A) = -Vc/2 + sqrt(Vc/2^2 + T/(2*ρ*A))
-area(r) = r^2*π
-phi(r, vc, vi, rpm) = atan((vc + vi)/((2π/60)*rpm*r))
-lambda(r, vc, vi, rpm) = phi(r, vc, vi, rpm)*r
-slope(r) = 2π*r
-pitch(r, at, ar) = deg2rad(at) - deg2rad(at - ar)*(r)
 
-vi = induced_velocity(vc, T, ρ, area(radius))
-r = [0.25:0.01:1.0;]
-λ = lambda.(r, vc, vi, rpm)
-ϕ = phi.(r,  vc, vi, rpm)
-a = slope.(r)
-θ = pitch.(r, 5, 2)
-# sig = solidity(r)
+r = radius.*[0.25:0.01:1.0;]
+θ = linear_twist.(r, 15, 2)
+vi = 0.005
 
-dCt = thrust_coefficient.(σ, a, θ, r, λ)
-Ct = integrate(dCt, r)
-T = integrate(dCt, r)*area(radius)*ρ*v_tip^2
-CtUI = T/(ρ*n^2*(diameter)^4)
+vi = similar(r)
+for i ∈ eachindex(r)
+    args = (vc, rpm, r[i], θ[i], cl, cd, chord, ρ)
+    vi[i] = secant_solver(
+        element_momentum_balance, 0.0, 0.0, v_tip/2, args=args)
+end
 
-U_corr = zeros(length(r))
-alpha = zeros(length(r))
-alphai = zeros(length(r))
 
-alphai .= induced_angle.(vc, vi, rpm, r) 
-U_corr .= corrected_velocity.(vc, vi, rpm, r)
-alpha .= θ .- alphai
+alphai = induced_angle.(vc, vi, rpm, r) 
+U_corr = corrected_velocity.(vc, vi, rpm, r)
+alpha = θ .- alphai
 dT = thrust_element.(cl.(alpha), cd.(alpha), chord, U_corr, alphai, ρ)
 dTm = thrust_momentum.(vc, vi, r, ρ)
-T_new = integrate(dT, r)
+thrust = integrate(dT, r)
 
-plot(r, dT)
-plot!(r, dTm)
+# plot(r, dT)
+# plot!(r, dTm)
+# plot(r, vi)
+
+scatter!([J], [thrust], label="J = $J")
--- src/functions.jl
+++ src/functions.jl
@@ -1,6 +1,8 @@
 export load_aerofoil, sigma, thrust_coefficient, integrate
+export linear_twist
 export induced_angle, corrected_velocity
-export thrust_element, thrust_momentum
+export thrust_element, thrust_momentum, element_momentum_balance
+export secant_solver
 
 load_aerofoil(file; startline=0) = begin
     data = readdlm(file, ',', Float64, skipstart=startline-1)
@@ -8,6 +10,10 @@ load_aerofoil(file; startline=0) = begin
     cl = Spline1D(alpha, data[:,2]) # generate cl fit
     cd = Spline1D(alpha, data[:,3]) # generate cd fit
     cl, cd
+end
+
+linear_twist(r, a_root, a_tip) = begin
+    deg2rad(a_root) - deg2rad(a_root - a_tip)*(r)
 end
 
 sigma(chord, radius, nb) = begin
@@ -52,6 +58,15 @@ thrust_momentum(vc, vi, r, ρ) = begin
     4*π*ρ*(vc + vi)*vi*r # (rdr)
 end
 
+element_momentum_balance(vi, vc, rpm, r, θ, cl, cd, chord, ρ) = begin
+    ϕ = induced_angle(vc, vi, rpm, r) 
+    U_corr = corrected_velocity(vc, vi, rpm, r)
+    α = θ - ϕ
+    Te = thrust_element(cl(α), cd(α), chord, U_corr, ϕ, ρ)
+    Tm = thrust_momentum(vc, vi, r, ρ)
+    Te - Tm
+end
+
 integrate(fx, x) = begin
     n = length(fx)
     sum = zero(Float64)
@@ -59,4 +74,47 @@ integrate(fx, x) = begin
         sum += 0.5*(fx[i] + fx[i+1])*(x[i+1] - x[i])
     end
     sum 
+end
+
+"""
+secant_solver(func, funcValue, g1, g2; tol=1e-8)
+
+Find the value x for which a function `func` will return the target value `funcValue`, given lower and upper guesses, `g1` and `g2` to an absolute tolerance `tol` (set to 1e-8 as default)
+"""
+function secant_solver(
+    func, funcValue, g1, g2; tol=1e-8, args=(), show=false)
+    
+    g = [g1:g2/10:g2;]
+    fg = zeros(length(g))
+    for i ∈ eachindex(fg)
+        fg[i] = func(g[i], args...,) - funcValue
+    end
+    # fg = func.(g, args...,) .- funcValue
+    I = sortperm(fg)
+    g1 = g[I[1]]
+    g2 = g[I[2]]
+
+    f1 = func(g1, args...,) - funcValue
+    f2 = func(g2, args...,) - funcValue
+    
+    for i ∈ 1:20
+    g2, g1, f1 = secant_method(g1, g2, f1, f2)
+    f2 = func(g2, args...,) - funcValue
+        if abs(f2) <= tol
+            show ? println("Converged: ", i, " iterations.") : nothing
+            return g2
+        end
+    end
+    println("Convergence criterion not met!")
+end
+
+
+"""
+xn, x2, f2 = secant_method(x1, x2, f1, f2)
+
+Direct implementation of the secant method equation. Takes current guesses (`x1` amd `x2`) and their function evaluations (`f1` and `f2`), returning the next guess `xn` and the previous state (`x2`, `f2``)
+"""
+function secant_method(x1, x2, f1, f2)
+    xn = x2 - f2*(x2 - x1)/(f2 - f1)
+    return xn, x2, f2
 end
